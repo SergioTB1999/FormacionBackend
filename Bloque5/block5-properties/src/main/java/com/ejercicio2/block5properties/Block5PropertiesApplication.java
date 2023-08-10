24b86412d6df932afd693de45164e2ea2a7c7bae
Sergio Torres <sergio.tblanca@bosonit.com>
2023-08-10 17:28:33 +0200

    Subida ejercicio2 (Bloque5)

---
+++ Bloque5/block5-properties/src/main/java/com/ejercicio2/block5properties/Block5PropertiesApplication.java
@@ -0,0 +1,38 @@
+package com.ejercicio2.block5properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5PropertiesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5PropertiesApplication.class, args);
+	}
+
+	@Value("${greeting}")
+	private String nombre1;
+
+	@Value("${my.number}")
+	private  String nombre2;
+
+	@Value("${new.property:No tiene valor}")
+	private String nombre3;
+
+	/*@Value("${env.MYURL}")
+	private String nombre4;*/
+
+	@Value("${env.MYURL2:NO_tengo_valor}")
+	private String nombre5;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("El valor de greeting es: " + nombre1);
+		System.out.println("El valor de my.number es: " + nombre2);
+		System.out.println("El valor de new.property es: " + nombre3);
+		/*System.out.println("El valor de env.MYURL es: " + nombre4);*/
+		System.out.println("El valor de env.MYURL2 es: " + nombre5);
+	}
+}
