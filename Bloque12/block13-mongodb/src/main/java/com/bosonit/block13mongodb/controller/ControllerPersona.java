b003a8abd20c232f79773468da59f491d161f890
Sergio Torres <sergio.tblanca@bosonit.com>
2023-08-23 12:23:24 +0200

    Crud mongoDB

--- Bloque12/block13-mongodb/src/main/java/com/bosonit/block13mongodb/controller/ControllerPersona.java
+++ Bloque12/block13-mongodb/src/main/java/com/bosonit/block13mongodb/controller/ControllerPersona.java
@@ -4,11 +4,17 @@ package com.bosonit.block13mongodb.controller;
 import com.bosonit.block13mongodb.application.PersonaServiceImpl;
 import com.bosonit.block13mongodb.controller.dto.PersonInputDto;
 import com.bosonit.block13mongodb.controller.dto.PersonOutputDto;
+import com.bosonit.block13mongodb.errors.CustomError;
+import com.bosonit.block13mongodb.errors.EntityNotFoundException;
+import com.bosonit.block13mongodb.errors.UnprocessableEntityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/persona")
@@ -17,6 +23,18 @@ public class ControllerPersona {
 
     @Autowired
     PersonaServiceImpl personaService;
+
+    @ExceptionHandler
+    @ResponseStatus
+    public CustomError handleUnprocessableEntityException(UnprocessableEntityException ex){
+        return new CustomError(new Date(), HttpStatus.UNPROCESSABLE_ENTITY.value(), ex.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus
+    public CustomError handleEntityNotFoundException (EntityNotFoundException ex){
+        return new CustomError(new Date(), HttpStatus.UNPROCESSABLE_ENTITY.value(), ex.getMessage());
+    }
 
     @PostMapping
     public ResponseEntity<PersonOutputDto> addPerson(@RequestBody PersonInputDto personInputDto){
